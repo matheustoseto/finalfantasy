5af1fad4120f66f1135a3b646b68495478916414
Unknown <matheus.toseto@hotmail.com>
2018-04-11 22:57:45 -0300

    add craft

---
+++ SurviveWalk/Assets/Scripts/Inventario/Craft/Resource.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resource : MonoBehaviour {
+
+    public Utils.ResourceType type;
+    public GameObject item;
+    public int idItem;
+    public int qnt;
+
+    public Material onMouseOver;
+    public Material onMouseExit;
+
+    private bool playerEnter = false;
+    private bool mouseEnter = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if ("Player".Equals(other.tag))
+        {
+            playerEnter = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if ("Player".Equals(other.tag))
+        {
+            playerEnter = false;
+            GetComponent<Renderer>().material = onMouseExit;
+        }
+    }
+
+    private void OnMouseOver()
+    {
+        if (playerEnter)
+        {
+            GetComponent<Renderer>().material = onMouseOver;
+            mouseEnter = true;
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        if (playerEnter)
+        {
+            GetComponent<Renderer>().material = onMouseExit;
+            mouseEnter = false;
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        if (playerEnter && mouseEnter)
+        {
+            item.GetComponent<ItemResource>().idItem = idItem;
+            for (int i = 1; i <= qnt; i++)
+            {
+                Instantiate(item, transform.position + new Vector3(Random.Range(-0.9f,0.9f), 0, Random.Range(-0.9f, 0.9f)), transform.rotation);
+            }     
+            Destroy(gameObject);
+        }   
+    }
+}
---
+++ SurviveWalk/Assets/Scripts/Inventario/Craft/Utils.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Utils {
+
+    public enum ResourceType { Comida, Madeira, Ferro, Machado };
+
+    [System.Serializable]
+    public class CraftItem
+    {
+        public int idItem;
+        public List<CraftCombination> craftCombination;
+    }
+
+    [System.Serializable]
+    public class CraftCombination
+    {
+        public int idItem;
+        public int qnt;
+    }
+}
