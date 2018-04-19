ee83465cfd24f3043bca041bb58861c95a2d682a
Unknown <matheus.toseto@hotmail.com>
2018-04-18 22:38:50 -0300

    add first gameplay

--- SurviveWalk/Assets/Scripts/Inventario/Craft/Utils.cs
+++ SurviveWalk/Assets/Scripts/Inventario/Craft/Utils.cs
@@ -5,5 +5,5 @@ using UnityEngine;
 public class Utils {
 
     public enum ResourceType { Comida, Madeira, Ferro, Machado };
-    public enum HouseType { House1 = 0, House2 = 1 };
+    public enum HouseType { House1 = 0, House2 = 1, House3 = 2, House4 = 3 };
 }
