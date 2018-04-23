2ffab08363a21598a7fe0a9bb9205772bb16f973
MatheusCapelari <32314560+MatheusCapelari@users.noreply.github.com>
2018-04-23 12:12:59 -0300

    Basic IA
    Adicionado IA + NavMesh!

---
+++ SurviveWalk/Assets/Scripts/Enemy/EnemyController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour {
+
+    public float radius = 10f;
+
+    Vector3 enemyInitialPos;
+    Transform playerTarget;
+    NavMeshAgent agent;    
+   
+    // Use this for initialization
+    void Start () {
+
+        enemyInitialPos = transform.position;
+        playerTarget = PlayerManager.instance.player.transform;
+        agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float distance = Vector3.Distance(playerTarget.position, transform.position);
+
+        //Player in range = chase!
+        if(distance <= radius)
+        {
+            agent.SetDestination(playerTarget.position);
+            lookToPlayer();
+
+            if (distance <= agent.stoppingDistance)
+            {
+                //PUT Attack the player HERE!!
+                lookToPlayer();
+            }
+        }
+
+        //Return to original spawn point + put full HP
+        else
+        {
+            agent.SetDestination(enemyInitialPos);
+            //PUT FULL HP HERE!!
+        }           
+	}
+
+    //Looks to player
+    void lookToPlayer()
+    {
+        Vector3 direction = (playerTarget.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+    }
+}
