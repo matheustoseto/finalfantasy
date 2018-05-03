2658478b9d741e0d2f5b9bc90bb5373f37bc413c
MatheusCapelari <32314560+MatheusCapelari@users.noreply.github.com>
2018-05-02 21:11:22 -0300

    New Scene
    ae

---
+++ SurviveWalk/Assets/Scripts/Personagem/CharacterMovementCapelari/IcarusController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IcarusController : MonoBehaviour {
+
+    public float runSpeed = 10;
+    public float turnSmoothTime = 0.2f;
+    public float speedSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+    float speedSmoothVelocity;
+    float currentSpeed;
+    float velocityY;
+
+    CharacterController controller;
+
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+
+    void Update()
+    {
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        Move(input);
+    }
+
+    void Move(Vector2 dir)
+    {
+        Vector2 inputDir = dir.normalized;
+
+        if (inputDir != Vector2.zero)
+        {
+            float targetRotation = Mathf.Atan2(inputDir.x, inputDir.y) * Mathf.Rad2Deg;
+            transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref turnSmoothVelocity, turnSmoothTime);
+        }
+
+        float targetSpeed = runSpeed * inputDir.magnitude;
+        currentSpeed = Mathf.SmoothDamp(currentSpeed, targetSpeed, ref speedSmoothVelocity, speedSmoothTime);
+
+        Vector3 velocity = transform.forward * currentSpeed + Vector3.up * velocityY;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
